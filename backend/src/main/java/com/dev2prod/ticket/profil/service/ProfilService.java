6a6e6c08c96fb742e47cb94a8d902f464a60dd10
fadili abdelhak <fadili101@gmail.com>
2023-08-24 23:10:49 +0100

    Ajout de workflow assignement

--- backend/src/main/java/com/dev2prod/ticket/profil/service/ProfilService.java
+++ backend/src/main/java/com/dev2prod/ticket/profil/service/ProfilService.java
@@ -6,15 +6,22 @@ import org.springframework.stereotype.Service;
 import com.dev2prod.ticket.caisse.entity.CaisseEntity;
 import com.dev2prod.ticket.profil.entity.ProfilEntity;
 import com.dev2prod.ticket.profil.repository.ProfilRepository;
+import com.dev2prod.ticket.user.entity.UserEntity;
+import com.dev2prod.ticket.workflow.entity.WorkflowEntity;
+import com.dev2prod.ticket.workflow.repository.WorkflowRepository;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ProfilService {
 
     @Autowired
     private ProfilRepository profilRepository;
-
+    @Autowired
+    private WorkflowRepository workflowRepository;
+    
     public ProfilEntity getProfilById(Long profilId) {
         Optional<ProfilEntity> profilOpt = profilRepository.findById(profilId);
         return profilOpt.orElse(null);
@@ -39,5 +46,14 @@ public class ProfilService {
         } else {
             return false;
         }
+    }
+    public ProfilEntity assignWorkflowToProfil(Long profilId, Long workflowId) {
+        Set<WorkflowEntity> workflowSet = null;
+        ProfilEntity profil = profilRepository.findById(profilId).get();
+        WorkflowEntity workflow = workflowRepository.findById(workflowId).get();
+        workflowSet =  profil.getAssignedWorkflows();
+        workflowSet.add(workflow);
+        profil.setAssignedWorkflows(workflowSet);
+        return profilRepository.save(profil);
     }
 }
